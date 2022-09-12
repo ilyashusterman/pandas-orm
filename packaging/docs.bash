a6f156d54b96d3506bfacea5ffab47e062ded47b
ilya shusterman <shusterilyaman@gmail.com>
2022-09-12 20:55:33 +0300

    fix: docs

--- packaging/docs.bash
+++ packaging/docs.bash
@@ -1,4 +1,1 @@
-pdoc src/pandas_orm/ !src/pandas_orm/base !src/pandas_orm/dataframe !src/pandas_orm/django/mixins \
- !src/pandas_orm/django/db !src/pandas_orm/sqlalchemy/execute_sql \
- !src/pandas_orm/sqlalchemy/model_dataframe_manager !src/pandas_orm/sqlalchemy/model_save_arguments \
- !src/pandas_orm/sqlalchemy/exceptions
+pdoc src/pandas_orm
